5e8d61cfcea79dade7b305c0c009d6b8480eccc8
Diago <83084169+ajmalyaseen@users.noreply.github.com>
2021-05-31 07:19:08 +0530

    Create command.py

---
+++ plugins/command.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+
+from pyrogram import filters, Client
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from bot import Translation # pylint: disable=import-error
+
+@Client.on_message(filters.command(["start"]) & filters.private, group=1)
+async def start(bot, update):
+    buttons = [[
+        InlineKeyboardButton("📫UPDATES", url="https://t.me/CoderzHEX"),
+        InlineKeyboardButton("🕵‍♂CREATOR", url="https://t.me/DIAGO_X")
+        ],[
+        InlineKeyboardButton("📕ABOUT", callback_data= "about"),
+        InlineKeyboardButton("🔐 CLOSE", callback_data= "close")
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.START_TEXT.format(
+                update.from_user.first_name),
+        reply_markup=reply_markup,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )
+    
+    
+@Client.on_message(filters.command(["about"]) & filters.private, group=1)
+async def about(bot, update):
+    
+    buttons = [[
+        InlineKeyboardButton('⬇️BACK', callback_data='start'),
+        InlineKeyboardButton('🔐CLOSE', callback_data='close')
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.ABOUT_TEXT,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )
+
+
+    
